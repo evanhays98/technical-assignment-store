f46045043c228eef34da309b5a542a8abd1aef79
evanhays98 <evan.hays@epita.fr>
2024-07-13 12:20:56 +0200

    implement basic read/write operations for Store

--- src/store.ts
+++ src/store.ts
@@ -1,52 +1,87 @@
-import { JSONArray, JSONObject, JSONPrimitive } from "./json-types";
+import {JSONArray, JSONObject, JSONPrimitive} from "./json-types";
 
 export type Permission = "r" | "w" | "rw" | "none";
 
 export type StoreResult = Store | JSONPrimitive | undefined;
 
 export type StoreValue =
-  | JSONObject
-  | JSONArray
-  | StoreResult
-  | (() => StoreResult);
+    | JSONObject
+    | JSONArray
+    | StoreResult
+    | (() => StoreResult);
 
 export interface IStore {
-  defaultPolicy: Permission;
-  allowedToRead(key: string): boolean;
-  allowedToWrite(key: string): boolean;
-  read(path: string): StoreResult;
-  write(path: string, value: StoreValue): StoreValue;
-  writeEntries(entries: JSONObject): void;
-  entries(): JSONObject;
+    defaultPolicy: Permission;
+
+    allowedToRead(key: string): boolean;
+
+    allowedToWrite(key: string): boolean;
+
+    read(path: string): StoreResult;
+
+    write(path: string, value: StoreValue): StoreValue;
+
+    writeEntries(entries: JSONObject): void;
+
+    entries(): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
+export function Restrict(permission?: Permission): any {
+    return function (target: any, key: string) {
+        if (!target.__permissions__) {
+            target.__permissions__ = {};
+        }
+        target.__permissions__[key] = permission;
+    };
 }
 
 export class Store implements IStore {
-  defaultPolicy: Permission = "rw";
+    private readonly data: Record<string, StoreValue> = {};
+    private readonly permissions: Record<string, Permission> = {};
+    defaultPolicy: Permission = "rw";
 
-  allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
-  }
+    constructor() {
+        const prototypes = Object.getPrototypeOf(this);
+        this.permissions = prototypes.__permissions__;
+        this.data = {};
+    }
 
-  allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
-  }
+    allowedToRead(key: string): boolean {
+        const permission = this.permissions[key] || this.defaultPolicy;
+        return permission.includes("r");
+    }
 
-  read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
-  }
+    allowedToWrite(key: string): boolean {
+        const permission = this.permissions[key] || this.defaultPolicy;
+        return permission.includes("w");
+    }
 
-  write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
-  }
+    read(path: string): StoreResult {
+        const splitPath = path.split(":");
+        const mainPath = splitPath[0]
+        let currentValue = this.data[splitPath[0]];
+        if (!this.allowedToRead(path)) {
+            throw new Error("Permission denied");
+        }
+        return currentValue as StoreResult;
+    }
 
-  writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
-  }
+    write(path: string, value: StoreValue): StoreValue {
+        if (typeof value === "function") {
+            value = value();
+        }
+        if (!this.allowedToWrite(path)) {
+            throw new Error("Permission denied");
+        }
+        this.data[path] = value;
+        return value;
+    }
 
-  entries(): JSONObject {
-    throw new Error("Method not implemented.");
-  }
+    writeEntries(entries: JSONObject): void {
+        throw new Error("Method not implemented.");
+    }
+
+    entries(): JSONObject {
+        throw new Error("Method not implemented.");
+    }
 }
